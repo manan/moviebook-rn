5a30660db1ce30264326994f2ab248f87e2f778a
Manan Mehta <mehtamanan@icloud.com>
2017-04-14 23:47:59 -0700

    create two action creators for loginsuccess and loginfail

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import {
   USERNAME_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAILED
+  LOGIN_USER_FAIL
 } from '../utils/types';
 
 
@@ -24,4 +24,12 @@ export const passwordChanged = (password) => {
 
 export const loginUser = ({ username, password }) => {
 
+}
+
+const loginUserSuccess = (dispatch, token) {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: token })
+}
+
+const loginUserFail = (dispatch, errors) {
+  dispatch({ type: LOGIN_USER_FAILED, payload: errors })
 }
