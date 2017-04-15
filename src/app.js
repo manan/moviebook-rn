5e7e185775dbcba1e88a06acdd01547bb4fddf3f
Manan Mehta <mehtamanan@icloud.com>
2017-04-14 21:44:15 -0700

    wire up redux-thunk in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import LoginScreen from './scenes/LoginScreen';
+import ReduxThunk from 'redux-thunk';
 
 const App = () => {
   return (
-    <Provider store={ createStore(reducers) }>
+    <Provider store={ createStore(reducers, {}, applyMiddleware(ReduxThunk)) }>
       <LoginScreen />
     </Provider>
   )
