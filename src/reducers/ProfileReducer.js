3f125131028e01b50dde94afd6d0bc9f55ce8082
Manan Mehta <mehtamanan@icloud.com>
2017-04-17 17:27:59 -0700

    add ProfileReducer

---
+++ src/reducers/ProfileReducer.js
@@ -0,0 +1,30 @@
+import {
+  SET_PROFILE_DETAILS,
+  LOG_OUT
+} from '../utils/types';
+
+const INITIAL_STATE = {
+  bio: '',
+  birth_date: '',
+  gender: '',
+  profile_picture: ''
+}
+
+export default (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+  console.log(action);
+  switch (type) {
+    case LOG_OUT:
+      return { ...INITIAL_STATE }
+    case SET_PROFILE_DETAILS:
+      return {
+        ...state,
+        bio: payload.bio,
+        birth_date: payload.birth_date,
+        gender: payload.gender,
+        profile_picture: payload.profile_picture,
+      }
+    default:
+      return state;
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import ProfileReducer from './ProfileReducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  profile: ProfileReducer
 })
