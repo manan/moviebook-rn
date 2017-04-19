df0f7e8601dd4d92d6997542022df55d4f6ad084
Manan Mehta <mehtamanan@icloud.com>
2017-04-19 06:20:20 -0700

    newsfeed set up well

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import ReactNative, { Text, View, StyleSheet, Dimensions } from 'react-native';
 import Image from 'react-native-image-progress';
 import ProgressBar from 'react-native-progress/Bar'; // eslint-disable-line
-import { sharedStyles } from '../utils';
+import { sharedStyles, images } from '../utils';
 import { Section } from '../components';
 
-const Post = ({ post }) => {
-  return (
-    <View style={{ flex: 1, paddingTop: 5, paddingBottom: 5 }}>
-      <Section style={styles.sideMargins}>
-        <Image
-          style={{ width: 30, height: 30, borderRadius: 15 }}
-          indicator={ProgressBar}
-          source={{ uri: post.profile_picture }}
-        />
-        <View
-          style={{ justifyContent: 'space-between', alignItems: 'flex-start', paddingLeft: 15 }}
-        >
-          <Text style={sharedStyles.simpleFontStyle} numberOfLines={1}>
-            {post.username}
-          </Text>
-          <Text style={sharedStyles.smallFontStyle} numberOfLines={1}>
-            {post.movie_title}
-          </Text>
-        </View>
-      </Section>
-    </View>
-  )
+class Post extends Component {
+  state = { width: 0, height: 0 }
+
+  componentDidMount() {
+    ReactNative.Image.getSize(this.props.post.poster_url, (srcWidth, srcHeight) => {
+      const maxHeight = Dimensions.get('window').height; // or something else
+      const maxWidth = Dimensions.get('window').width;
+
+      const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+      this.setState({ width: srcWidth * ratio, height: srcHeight * ratio });
+    }, error => {
+      console.log('error:', error);
+    });
+  }
+
+
+  render() {
+    const { post } = this.props;
+    return (
+      <View style={{ flex: 1, paddingTop: 5, paddingBottom: 5 }}>
+        <Section style={styles.sideMargins}>
+          <Image
+            style={{ width: 30, height: 30, borderRadius: 15 }}
+            source={{ uri: post.profile_picture }}
+            defaultSource={images.avatar}
+          />
+          <View
+            style={{ justifyContent: 'space-between', alignItems: 'flex-start', paddingLeft: 15 }}
+          >
+            <Text style={sharedStyles.simpleFontStyle} numberOfLines={1}>
+              {post.username}
+            </Text>
+            <Text style={sharedStyles.smallFontStyle} numberOfLines={1}>
+              {post.movie_title}
+            </Text>
+          </View>
+        </Section>
+
+        <Section style={{ paddingTop: 5, paddingBottom: 10 }}>
+          <Image
+            style={{ flex: 1, width: this.state.width, height: this.state.height }}
+            indicator={ProgressBar}
+            source={{ uri: post.poster_url }}
+            defaultSource={images.grey}
+          />
+        </Section>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
--- src/utils/images.js
+++ src/utils/images.js
@@ -1,6 +1,8 @@
 const images = {
   logo: require('../images/logo.jpg'), // eslint-disable-line
-  icon: require('../images/icon.jpg') // eslint-disable-line
+  icon: require('../images/icon.jpg'), // eslint-disable-line
+  avatar: require('../images/avatar.jpg'), // eslint-disable-line
+  noimage: require('../images/noimage.jpg') // eslint-disable-line
 };
 
 export { images };
